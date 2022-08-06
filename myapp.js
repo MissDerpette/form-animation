24443c358a6f7e3ba37095e30fd9c1f1974d7c6a
MissDerpette <missderpette@shadow.local>
2022-08-06 17:18:44 +0800

    js incomplete

---
+++ myapp.js
@@ -0,0 +1,16 @@
+function animatedForm() {
+    const arrows = document.querySelectorAll(".fa-arrow-down");
+
+    arrows.forEach(arrow => { arrow.addEventListener ("click", () => {
+        const input = arrow.previousElementSibling;
+        const parent = arrow.parentElement;
+        console.log(parent);
+     });
+    });
+
+
+}
+
+
+
+animatedForm();
